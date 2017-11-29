a9c71adf82bc2ad0daae2caf7125f5580cc1f557
Tony Xu <xuology@gmail.com>
2017-11-29 14:36:01 -0500

    add webpack config, add template for client files

---
+++ client/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import configureStore from './store/store';
+// import initialState from './reducers/initialState';
+// import App from './router/App.jsx';
+
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
