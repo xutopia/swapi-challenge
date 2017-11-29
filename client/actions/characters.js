d7448284d272707feb114755023e4ab817e30dba
Tony Xu <xuology@gmail.com>
2017-11-29 15:14:18 -0500

    add selet and clear character actions

---
+++ client/actions/characters.js
@@ -0,0 +1,10 @@
+import { createAction } from 'redux-actions';
+import axios from 'axios';
+
+
+export const selectCharacter = createAction('selectCharacter', () => {
+    return 0;
+});
+
+
+export const clearSelected = createAction('clearSelected');
