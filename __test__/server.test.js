b3dac730764773453261dfa8fd01efe789ee44ef
Tony Xu <xuology@gmail.com>
2017-11-29 14:25:31 -0500

    add server file, test should fail as there is no static asset to serve

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var expect = require('chai').expect;
-var app = require('../../server/server.js');
+var app = require('../server/server.js');
 var agent = request.agent(app);
 
 describe('server tests', function() {
@@ -16,5 +16,4 @@ describe('server tests', function() {
         done();
       });
   });
-  
 });
